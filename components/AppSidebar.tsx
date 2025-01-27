6c436fc55fc834824d9f6cf05a30ee58f66c1ec4
DBH <dbhurley@me.com>
2025-01-27 17:35:01 +0000

    updated sidebar

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -360,7 +360,7 @@ function AppSidebar() {
     <Sidebar>
       <SidebarHeader>
         <div className="flex justify-between p-2 pb-0">
-          <span className="text-lg font-semibold text-red-400">Gemini Next Chat</span>
+          <span className="text-lg font-semibold text-red-400">Molly Factory</span>
           <Button
             className="h-8 w-8 [&_svg]:size-5"
             variant="ghost"
